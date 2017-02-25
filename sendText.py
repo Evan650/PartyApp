a38052ccb57573fc1f2dc6927d53dedf356153dd
Kennychiwa <Kennychiwa@colorado.edu>
2017-02-25 16:38:07 -0700

    adding json abilities

--- sendText.py
+++ sendText.py
@@ -1,11 +1,15 @@
+import json
 from twilio.rest import TwilioRestClient
+
+json_data=open("data.json").read()
 
 
 client = TwilioRestClient(account='AC1886dcb01585fe153b03644681df3ef4',
                               token='732134a0980da87ca9b621b96966f085')
 
 client.messages.create(from_= "(720) 709-2497",
-			to = "(720) 291-6454",
+			to = people.kenny_number,
 			body = "Fuck You")
+
 
 
