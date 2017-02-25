60b6b2b4251fac8d261ce3f87f2ffb37112a3e2e
Angela Werner <AngelaWerner@rgnt2-115-35-dhcp.int.colorado.edu>
2017-02-25 13:30:49 -0700

    Added two tables to database

--- database.sql
+++ database.sql
@@ -1,2 +1,16 @@
 #Our database..
 #By: Evan Yin, Angela Werner Kenny Ellis
+CREATE TABLE 'UserInfo'.'tbl_user' (
+'user_uid' BIGINT UNIQUE AUTO_INCREMENT,
+'last_name' VARCHAR(45) NULL,
+'first_name' VARCHAR(45) NULL,
+'phone' INT(10),
+PRIMARY KEY ('user_uid'));
+
+CREATE TABLE 'UserInfo'.'tbl_address' (
+'address_uid' BIGINT UNIQUE AUTO_INCREMENT,
+'address' VARCHAR(45) NULL,
+'city' VARCHAR(45) NULL,
+'country' VARCHAR(45) NULL,
+'postal code' INT() NULL,
+PRIMARY KEY ('address_uid'));
