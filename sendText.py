a5b0fc65c39cde886ee3ebefd47f56b190948e62
Angela Werner <AngelaWerner@rgnt2-115-35-dhcp.int.colorado.edu>
2017-02-25 15:40:34 -0700

    Added text functionality

--- sendText.py
+++ sendText.py
@@ -1,8 +1,8 @@
 from twilio.rest import TwilioRestClient
 
 
-client = TwilioRestClient(account='AC38135355602040856210245275870',
-                              token='2flnf5tdp7so0lmfdu3d')
+client = TwilioRestClient(account='AC1886dcb01585fe153b03644681df3ef4',
+                              token='732134a0980da87ca9b621b96966f085')
 
 client.messages.create(from_= "(720) 709-2497",
 			to = "(720) 291-6454",
