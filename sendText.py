a61c3cb2908dca6e06c3d13687f8dfe1386d5199
Kennychiwa <Kennychiwa@colorado.edu>
2017-02-25 15:35:11 -0700

    updated constructor

--- sendText.py
+++ sendText.py
@@ -1,6 +1,8 @@
 from twilio.rest import TwilioRestClient
 
-client = TwilioRestClient()
+
+client = TwilioRestClient(account='AC38135355602040856210245275870',
+                              token='2flnf5tdp7so0lmfdu3d')
 
 client.messages.create(from_= "(720) 709-2497",
 			to = "(720) 291-6454",
