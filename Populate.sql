e745f2749947be79796c35b4b4f10e56fb4456f2
Evan Yin <Evan@rgnt2-76-96-dhcp.int.colorado.edu>
2017-02-25 15:03:04 -0700

    fixed syntax etc for .sql files

--- Populate.sql
+++ Populate.sql
@@ -2,13 +2,13 @@
 #By Evan Yin, Angela Werner, Kenny Ellis
 
 INSERT INTO tbl_user (last_name , first_name, phone)
-VALUES;
-  (Werner, Angela, 7209389660),
-  (Chen, Evan, 7204125686),
-  (Ellis, Kenny, 7202916454);
+VALUES
+  ('Werner', 'Angela', '7209389660'),
+  ('Chen', 'Evan', '7204125686'),
+  ('Ellis', 'Kenny', '7202916454');
 
 INSERT INTO tbl_address (address, city, state, country, postal_code)
 VALUES;
-  (1122 Pleasent Street Unit B, Boulder, Colorado, United States, 80302),
-  (1036 12th Street, Boulder, Colorado, United States, 80302),
-  (705 20th Street, Boulder, Colorado, United States, 80302),
+  ('1122 Pleasent Street Unit B', 'Boulder', 'Colorado', 'United States', 80302),
+  ('1036 12th Street, Boulder', 'Colorado', 'United States', 80302),
+  ('705 20th Street, Boulder', 'Colorado', 'United States', 80302),
--- database.sql
+++ database.sql
@@ -3,18 +3,18 @@
 
 
 
-CREATE TABLE 'UserInfo'.'tbl_user' (
-'user_uid' BIGINT UNIQUE AUTO_INCREMENT,
-'last_name' VARCHAR(45) NULL,
-'first_name' VARCHAR(45) NULL,
-'phone' INT(10),
-PRIMARY KEY ('user_uid'));
+CREATE TABLE UserInfo.tbl_user (
+user_uid BIGINT UNIQUE AUTO_INCREMENT,
+last_name VARCHAR(45) NULL,
+first_name VARCHAR(45) NULL,
+phone INT(10),
+PRIMARY KEY (user_uid));
 
-CREATE TABLE 'UserInfo'.'tbl_address' (
-'address_uid' BIGINT UNIQUE AUTO_INCREMENT,
-'address' VARCHAR(45) NULL,
-'city' VARCHAR(45) NULL,
-'state' VARCHAR(20) NULL,
-'country' VARCHAR(45) NULL,
-'postal_code' INT(5) NULL,
-PRIMARY KEY ('address_uid'));
+CREATE TABLE UserInfo.tbl_address (
+address_uid BIGINT UNIQUE AUTO_INCREMENT,
+address VARCHAR(45) NULL,
+`city` VARCHAR(45) NULL,
+state VARCHAR(20) NULL,
+country VARCHAR(45) NULL,
+postal_code INT(5) NULL,
+PRIMARY KEY (address_uid));
