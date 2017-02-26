c63f91f01365a311339ab62954c334853d071420
Evan Yin <Evan@rgnt2-76-96-dhcp.int.colorado.edu>
2017-02-26 09:56:59 -0700

    hi

--- sendText.py
+++ sendText.py
@@ -36,9 +36,8 @@ parsed_json=json.dumps(people_json)
 
 client = TwilioRestClient(account='AC1886dcb01585fe153b03644681df3ef4',
                              token='732134a0980da87ca9b621b96966f085')
-
+#the 720-709-2497 number is the twilio free number
+#the twilio free account
 client.messages.create(from_= "(720) 709-2497",
 			to = people_json["people"][1]["number"],
  			body = "Fuck You")
-
-
