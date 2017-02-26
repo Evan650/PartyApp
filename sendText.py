9f4c6547f59499fc646268ed65897af603378053
Kennychiwa <Kennychiwa@colorado.edu>
2017-02-25 17:14:53 -0700

    adding json implementation

--- sendText.py
+++ sendText.py
@@ -1,15 +1,42 @@
 import json
 from twilio.rest import TwilioRestClient
 
-json_data=open("data.json").read()
 
 
-client = TwilioRestClient(account='AC1886dcb01585fe153b03644681df3ef4',
-                              token='732134a0980da87ca9b621b96966f085')
+people_json = {
+    "people": [
+        {
+            "number": "7204125686",
+            "fname": "Evan",
+            "lname": "Yin",
+            "location": "Boulder"
+        },
+        {
+            "number": "7202916454",
+            "fname": "Kenny",
+            "lname": "Ellis",
+            "location": "Boulder"
+        },
+        {
+            "number": "7209389660",
+            "fname": "Angela",
+            "lname": "Werner",
+            "location": "Boulder"
+        }
+    ]
+}
+parsed_json=json.dumps(people_json)
 
-client.messages.create(from_= "(720) 709-2497",
-			to = people.kenny_number,
-			body = "Fuck You")
+print(people_json.people.number)
 
+
+
+
+# client = TwilioRestClient(account='AC1886dcb01585fe153b03644681df3ef4',
+#                               token='732134a0980da87ca9b621b96966f085')
+
+# client.messages.create(from_= "(720) 709-2497",
+# 			to = people.kenny_number,
+# 			body = "Fuck You")
 
 
